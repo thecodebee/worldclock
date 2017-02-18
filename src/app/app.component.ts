ed7d481ab55b2bdff17e76da3f3a3b7022de971a
bhuvanarajamani <bhuvana.cr@gmail.com>
2017-02-18 14:46:55 -0600

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy, OnChanges} from '@angular/core';
 import * as moment from 'moment-timezone';
 
 const TZ_FORMAT = "YYYY-MM-DD HH:mm:ss";
-const TZ_FORMAT_SHORT = "ddd HH:mm:ss";
+const TZ_FORMAT_SHORT = "ddd HH:mm";
 
 @Component({
   selector: 'app-root',
@@ -12,114 +12,108 @@ const TZ_FORMAT_SHORT = "ddd HH:mm:ss";
 })
 
 export class AppComponent implements OnInit{
-  tz_local: string ;
-  tz_newYork: string;
-  tz_losAngeles: string;
-  tz_london: string;
-  tz_chennai: string;
-  tz_melbourne: string;
-  slider_max : number = 0;
-  val: number = 0;
-  zone1: number = 0;
-  zone2: number = 0;
-  local_unix: any;
-  zone1_unix: any;
-  zone2_unix: any;
-  zone1_offset : number = 750;
-  zone2_offset : number = 420;
 
   timestamp: any;
+  localMoment: any;
+  chennaiMoment: any;
+  MelbourneMoment: any;
+
+  timezoneStringLocal: string;
+  timezoneStringChennai: string;
+  timezoneStringMelbourne: string;
+
+  formattedTimeLocal: string ;
+  formattedTimeChennai: string;
+  formattedTimeMelbourne: string;
+
+  sliderValueLocal: number = 0;
+  sliderValueChennai: number = 0;
+  sliderValueMelbourne: number = 0;
+
+  sliderFormattedTimeLocal: string;
+  sliderFormattedTimeChennai: string;
+  sliderFormattedTimeMelbourne: string;
+ 
+  slider_max : number = 24*60; // minutes 
 
   constructor(){
+    this.timezoneStringLocal = moment.tz.guess();
+    this.timezoneStringChennai = "Asia/Kolkata";
+    this.timezoneStringMelbourne = "Australia/Melbourne";
     this.showTime();
   }
 
   ngOnInit(){
     this.showTime();
-    setInterval( () => {
-      this.showTime();
-    }
-      , 1000);
+    // setInterval( () => {
+    //   this.showTime();
+    // }
+    //   , 1000);
   }
 
   showTime(){
     this.timestamp = moment();
-    var local       = moment.tz(this.timestamp,moment.tz.guess());
-    var newYork     = local.clone().tz("America/New_York");
-    var losAngeles  = local.clone().tz("America/Los_Angeles");
-    var london      = local.clone().tz("Europe/London");
-    var chennai     = local.clone().tz("Asia/Kolkata");
-    var melbourne   = local.clone().tz("Australia/Melbourne");
+    this.localMoment       = moment.tz(this.timestamp,this.timezoneStringLocal);
+    this.chennaiMoment     = this.localMoment.clone().tz(this.timezoneStringChennai);
+    this.MelbourneMoment   = this.localMoment.clone().tz(this.timezoneStringMelbourne);
 
-    this.tz_local = local.format(TZ_FORMAT);
-    this.tz_newYork = newYork.format(TZ_FORMAT);    
-    this.tz_losAngeles = losAngeles.format(TZ_FORMAT); 
-    this.tz_london = london.format(TZ_FORMAT);  
-    this.tz_chennai = chennai.format(TZ_FORMAT);   
-    this.tz_melbourne = melbourne.format(TZ_FORMAT);    
+    this.formattedTimeLocal = this.localMoment.format(TZ_FORMAT);
+    this.formattedTimeChennai = this.chennaiMoment.format(TZ_FORMAT);   
+    this.formattedTimeMelbourne = this.MelbourneMoment.format(TZ_FORMAT);    
 
-    this.val = local.hour()*60+local.minute();
-    this.zone1 = chennai.hour()*60+chennai.minute();
-    this.zone2 = melbourne.hour()*60+melbourne.minute();
-    this.slider_max = 24*60; // minutes 
+    this.sliderValueLocal     = this.localMoment.hour()*60+this.localMoment.minute();
+    this.sliderValueChennai   = this.chennaiMoment.hour()*60+this.chennaiMoment.minute();
+    this.sliderValueMelbourne = this.MelbourneMoment.hour()*60+this.MelbourneMoment.minute();
     
-    this.local_unix = local.format(TZ_FORMAT_SHORT);
-    this.zone1_unix = chennai.format(TZ_FORMAT_SHORT);
-    this.zone2_unix = melbourne.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeLocal     = this.localMoment.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeChennai   = this.chennaiMoment.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeMelbourne = this.MelbourneMoment.format(TZ_FORMAT_SHORT);
   }
 
   updateTime(localTime: string){
-    var local       = moment.tz(moment(localTime),moment.tz.guess());
-    var newYork     = local.clone().tz("America/New_York");
-    var losAngeles  = local.clone().tz("America/Los_Angeles");
-    var london      = local.clone().tz("Europe/London");
-    var chennai     = local.clone().tz("Asia/Kolkata");
-    var melbourne   = local.clone().tz("Australia/Melbourne");
+    this.localMoment       = moment.tz(moment(localTime),this.timezoneStringLocal);
+    this.chennaiMoment     = this.localMoment.clone().tz(this.timezoneStringChennai);
+    this.MelbourneMoment   = this.localMoment.clone().tz(this.timezoneStringMelbourne);
 
-    this.tz_local = local.format(TZ_FORMAT);
-    this.tz_newYork = newYork.format(TZ_FORMAT);    
-    this.tz_losAngeles = losAngeles.format(TZ_FORMAT); 
-    this.tz_london = london.format(TZ_FORMAT);  
-    this.tz_chennai = chennai.format(TZ_FORMAT);   
-    this.tz_melbourne = melbourne.format(TZ_FORMAT); 
+    this.formattedTimeLocal     = this.localMoment.format(TZ_FORMAT);
+    this.formattedTimeChennai   = this.chennaiMoment.format(TZ_FORMAT);   
+    this.formattedTimeMelbourne = this.MelbourneMoment.format(TZ_FORMAT);
   }
 
-  handleSliderChange(e, trigger:string) {
-    var slider_val = null;
-    var moment_from_slider = null;
-    var local;
-    var chennai;
-    var melbourne;
+  handleSliderChange(e, timezone:string) {
+    var slider_val : any;
+    var moment_from_slider : any;
 
     slider_val = e.value;
+    console.log("slider value ==> ", slider_val);
     moment_from_slider = moment({hour: slider_val/60, minute: slider_val % 60});
     
-    switch (trigger) {
+    switch (timezone) {
       case "local": 
-        local       = moment.tz(moment(moment_from_slider),moment.tz.guess());
-        chennai     = local.clone().tz("Asia/Kolkata");
-        melbourne   = local.clone().tz("Australia/Melbourne");
+        this.localMoment       = moment.tz(moment_from_slider,this.timezoneStringLocal);
+        this.chennaiMoment     = this.localMoment.clone().tz(this.timezoneStringChennai);
+        this.MelbourneMoment   = this.localMoment.clone().tz(this.timezoneStringMelbourne);
         break;
       case "chennai": 
-        chennai     = moment.tz(moment(moment_from_slider),"Asia/Kolkata");
-        local       = chennai.clone().tz(moment.tz.guess());
-        melbourne   = chennai.clone().tz("Australia/Melbourne");
+        this.chennaiMoment     = moment.tz(moment_from_slider,this.timezoneStringChennai);
+        this.localMoment       = this.chennaiMoment.clone().tz(this.timezoneStringLocal);
+        this.MelbourneMoment   = this.chennaiMoment.clone().tz(this.timezoneStringMelbourne);
         break;
       case "melbourne":
-        melbourne   = moment.tz(moment(moment_from_slider),"Australia/Melbourne");
-        local       = melbourne.clone().tz(moment.tz.guess());
-        chennai     = melbourne.clone().tz("Asia/Kolkata");
+        this.MelbourneMoment   = moment.tz(moment_from_slider,this.timezoneStringMelbourne);
+        this.localMoment       = this.MelbourneMoment.clone().tz(this.timezoneStringLocal);
+        this.chennaiMoment     = this.MelbourneMoment.clone().tz(this.timezoneStringChennai);
         break;
       default:
         confirm("Sorry, that option is not in the system yet!");
     }
 
-    this.local_unix = local.format(TZ_FORMAT_SHORT);
-    this.zone1_unix = chennai.format(TZ_FORMAT_SHORT);
-    this.zone2_unix = melbourne.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeLocal = this.localMoment.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeChennai = this.chennaiMoment.format(TZ_FORMAT_SHORT);
+    this.sliderFormattedTimeMelbourne = this.MelbourneMoment.format(TZ_FORMAT_SHORT);
 
-    this.val = local.hour()*60+local.minute();
-    this.zone1 = chennai.hour()*60+chennai.minute();
-    this.zone2 = melbourne.hour()*60+melbourne.minute();
+    this.sliderValueLocal = this.localMoment.hour()*60+this.localMoment.minute();
+    this.sliderValueChennai = this.chennaiMoment.hour()*60+this.chennaiMoment.minute();
+    this.sliderValueMelbourne = this.MelbourneMoment.hour()*60+this.MelbourneMoment.minute();
   }
 }
